3f523908e89a3a81b20cba32464776582aa2d44c
AndreAro04 <amalore.andre@gmail.com>
2018-06-19 16:59:23 +0200

    Start_project
    first_commit

---
+++ Assets/Scripts_catalogue/AddFile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddFile : MonoBehaviour
+{
+	int count = 1;
+
+	public void TaskOnClickAddButton()
+	{
+		Instantiate (GameObject.Find (this.GetComponentInChildren<Text> ().text), Camera.main.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 90.0f)), Quaternion.identity);
+		count++;
+		this.gameObject.transform.Find ("Button_total").gameObject.GetComponentInChildren<Text> ().text = count.ToString ();
+	}
+}
+
+
---
+++ Assets/Scripts_catalogue/LoadFile.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class LoadFile : MonoBehaviour 
+{
+	string path = null;
+	public Button nameButton;
+	public GameObject nameContentPanel;
+
+	private GameObject loadedFile;
+	private string fileName;
+	private Button namBut;
+	private List<string> listGoName;
+
+	void Start()
+	{
+		listGoName = new List<string>();
+		fileName = "";
+	} 
+
+	public void TaskOnClickFile()
+	{
+		path = EditorUtility.OpenFilePanel ("Choose your .obj file", "", "obj");	
+		loadedFile = OBJLoader.LoadOBJFile (path);
+
+		if (!listGoName.Contains(loadedFile.name)) 
+		{
+			namBut = Instantiate (nameButton, nameContentPanel.transform);
+			fileName = loadedFile.name;
+			namBut.GetComponentInChildren<Text> ().text = fileName;
+			namBut.transform.Find ("Button_total").gameObject.GetComponentInChildren<Text> ().text = 1.ToString();
+			listGoName.Add(loadedFile.name);
+		} 
+
+		else if (listGoName.Contains(loadedFile.name))
+		{
+			Debug.Log("file already exists");
+			Destroy (loadedFile);
+		}
+	
+	}
+}
